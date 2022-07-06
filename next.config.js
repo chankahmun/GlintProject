c08f106f5d58df6aae49f27f62c76c9413738b61
chankahmun <45312550+chankahmun@users.noreply.github.com>
2022-07-06 16:54:57 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,56 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+// const withSourceMaps = require("@zeit/next-source-maps");
+// const withImages = require("next-images");
+// const withPlugins = require('next-compose-plugins');
+// const withTM = require('next-transpile-modules')(['ThePackage']);
+
+// module.exports = withPlugins([
+//     withTM,
+//     [
+//         withImages,
+//         {
+//             exclude: /\.svg$/
+//         }
+//     ],
+//     withSourceMaps
+// ],
+// {
+    
+//     webpack: (config, options) => {
+//       if (!options.isServer) {
+//         config.resolve.alias["@sentry/node"] = "@sentry/browser";
+//       }
+//       config.module.rules.push({
+//         test: /\.svg$/,
+//         use: ["@svgr/webpack"]
+//       });
+//       return config;
+//     }
+//   });
+
+const withTranspileModules = require('next-transpile-modules')([
+  'glints-aries',
+]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      type: "asset",
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = withTranspileModules(nextConfig);
